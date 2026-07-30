2796266977057af605bf941ea5cb9fc9f3ff2df4
iingjeong <silvmin42@gmail.com>
2026-07-30 18:04:49 +0900

    feat: DomesticTrade 도메인 DTO 추가

---
+++ src/main/java/com/app/returns/domain/dto/DomesticTradeDTO.java
@@ -0,0 +1,19 @@
+package com.app.returns.domain.dto;
+
+import lombok.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter @ToString @Builder
+
+public class DomesticTradeDTO {
+    private Long tradeId;
+    private Long domesticAccountId;
+    private String tradeType;
+    private String stockCode;
+    private BigDecimal qty;
+    private BigDecimal price;
+    private LocalDateTime executedAt;
+}
