17026287873e0bf64add3370cf4e75056a6be00a
iingjeong <silvmin42@gmail.com>
2026-07-30 18:03:34 +0900

    feat: CopyForeignProduct 도메인 DTO 및 Type enum 추가

---
+++ src/main/java/com/app/returns/domain/dto/CopyForeignProductDTO.java
@@ -0,0 +1,17 @@
+package com.app.returns.domain.dto;
+
+import com.app.returns.domain.type.Type;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter @ToString @Builder
+
+public class CopyForeignProductDTO {
+    private Long foreignProductId;
+    private String ticker;
+    private String name;
+    private String market;
+    private String currency;
+    private Type type;
+}
